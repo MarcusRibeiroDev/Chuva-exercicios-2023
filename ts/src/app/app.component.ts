43c42a7002c2fdeb5d132cbef7995967f4e1c06c
MarcusRibeiroDev <marcus10dev@gmail.com>
2023-10-08 16:16:32 -0300

    Test create a new topic

--- ts/src/app/app.component.ts
+++ ts/src/app/app.component.ts
@@ -9,9 +9,16 @@ export class AppComponent {
   title = 'DevChuva';
   showRestText: boolean = false;
   showButton: boolean = true;
+  btnCreateTopic: boolean = false;
+  btnSubmitForm: boolean = false;
 
   toggleTextAndButton(): void {
     this.showRestText = !this.showRestText;
     this.showButton = !this.showButton;
+  }
+
+  toggleState(): void{
+    this.btnCreateTopic = !this.btnCreateTopic
+    this.btnSubmitForm = !this.btnSubmitForm
   }
 }
